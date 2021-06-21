845f22a9a638247fb77ee63a7ce4a2ac8708687e
Fokhrul Islam <fokhrul.shiblee@gmail.com>
2021-06-21 23:22:14 +0600

    Readme added

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { UserContext } from './../../App';
 import Navbar from './../Navbar/Navbar';
 import PostJob from './../PostJob/PostJob';
 import JobReview from './../JobReview/JobReview';
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
@@ -16,13 +17,14 @@ const Dashboard = () => {
     return (
         <div>
             <Navbar />
-            <div className="container">
+            <div className="container mt-3">
             <h5 className="text-center">Name : {userData.name}</h5>
             <h5 className="text-center">Email: {userData.email}</h5>
             <h5 className="text-center">Role : {userData.role}</h5>
             {userData.accountType && <h5 className="text-center">Account Type : {userData.accountType}</h5>}
             {userData.role === 'Employer' && <PostJob userData={userData} />}
             {userData.role === 'Admin' && <JobReview />}
+            {userData.role === 'Job Seeker' && <Link to='/' className="btn btn-primary" >Apply To Jobs</Link>}
         </div>
         </div>
     );
